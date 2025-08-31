635787c7d8007b985c5c14f17dcb593d5aa45317
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2025-08-31 11:55:37 +0530

    Update jasminCSlabQ14.c

--- jasminCSlabQ14.c
+++ jasminCSlabQ14.c
@@ -3,22 +3,13 @@
 int main() 
 {
     int a,b;
-    float c,d;
     printf("Enter positive integer value:");
     scanf("%d",&a);
     printf("Enter negative integer value:");
     scanf("%d",&b);
-    printf("Enter positive decimal value:");
-    scanf("%f",&c);
-    printf("Enter negative decimal value:");
-    scanf("%f",&d);
     printf("\nFloor value for positive integer a:%.2f",floor(a));
     printf("\nCeil value for positive integer a:%.2f",ceil(a));
     printf("\nFloor value for negative integer b:%.2f",floor(b));
     printf("\nCeil value for negative integer b:%.2f",ceil(b));
-    printf("\nFloor value for positive float c:%.2f",floor(c));
-    printf("\nCeil value for negative float c:%.2f",ceil(c));
-    printf("\nFloor value for positive float d:%.2f",floor(d));
-    printf("\nCeil value for negative float d:%.2f",ceil(d));
     return 0;
 }

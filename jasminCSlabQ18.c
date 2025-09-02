8c0b53286d9397c514fe6f2c5d3b36184c03196a
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2025-09-02 15:27:57 +0530

    Add files via upload

---
+++ jasminCSlabQ18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter number a:");
+	scanf("%d",&a);
+	printf("Enter number b:");
+	scanf("%d",&b);
+	if(a>b)
+	{
+		printf("%d is greater",a);
+	}
+	else
+	{
+		printf("%d is greater",b);
+	}
+	return 0;
+}
+

148300b646f84325abf3af06b64196a3d1176868
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2025-09-09 00:32:26 +0530

    Add files via upload

---
+++ jasminCSlabQ25.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, num3;
+    printf("Enter value for num1:");
+	scanf("%d",&num1);
+	printf("Enter value for num2:");
+	scanf("%d",&num2);
+	printf("Enter value for num3:");
+	scanf("%d",&num3);
+    if (num1 >= num2) { 
+        if (num1 >= num3) { 
+            printf("%d is the maximum number.\n", num1);
+        } else { 
+            printf("%d is the maximum number.\n", num3);
+        }
+    } else {
+        if (num2 >= num3) { 
+            printf("%d is the maximum number.\n", num2);
+        } else { 
+            printf("%d is the maximum number.\n", num3);
+        }
+    }
+
+    return 0; 
+}

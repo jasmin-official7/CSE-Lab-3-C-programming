3f9e93e8e760d828ac5c7330436d264374563825
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2025-09-09 00:13:11 +0530

    Update jasminCSlabQ21.c

--- jasminCSlabQ21.c
+++ jasminCSlabQ21.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int a;
-	printf("Enter a year:");
-	scanf("%d",&a);
-	if(a%4==0)
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+    if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) 
 	{
-		printf("%d is a leap year",a);
-	}
-	else
+        printf("%d is a leap year.\n", year);
+    }
+	else 
 	{
-		printf("%d is not a leap year",a);
-	}
-	return 0;
+        printf("%d is not a leap year.\n", year);
+    }
 
+    return 0;
 }

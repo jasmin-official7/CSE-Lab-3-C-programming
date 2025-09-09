9369dbe1415f888c275362c1d2ef7146644f7535
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2025-09-09 15:45:53 +0530

    Add files via upload

---
+++ jasminCSlabQ27.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+char x;
+printf("Enter an alphabet:");
+scanf("%c",&x);
+switch(x)
+{
+	case'a': printf("%c is a vowel",x);
+		break;
+	case'e': printf("%c is a vowel",x);
+		break;
+	case'i': printf("%c is a vowel",x);
+		break;
+	case'o':printf("%c is a vowel",x);
+		break;
+	case'u':printf("%c is a vowel",x);
+		break;
+	default:printf("%c is a consonant",x);
+}
+return 0;
+}
+

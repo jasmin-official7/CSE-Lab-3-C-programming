85ead020861933654e315868c4e96d1eb6ae1266
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2025-11-10 21:44:53 +0530

    Add files via upload

---
+++ jasminCSlabQ61.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(){
+	int arr_1[10][10], arr_2[10][10],arr_mul[10][10] ;
+	int i,j,n,m;
+	printf("Enter the number of rows (n): ");
+    scanf("%d", &n);
+    printf("Enter the number of columns (m): ");
+    scanf("%d", &m);
+    for(i=0;i<n;i++){
+    	for(j=0;j<m;j++){
+    		printf("Enter element for matrix 1 [%d][%d]: ", i, j);
+            scanf("%d",&arr_1[i][j]);
+		}
+	}
+	
+	for(i=0;i<n;i++){
+    	for(j=0;j<m;j++){
+    		printf("Enter element for matrix 2 [%d][%d]: ", i, j);
+            scanf("%d", &arr_2[i][j]);
+		}
+	}
+	
+	for(i=0;i<n;i++){
+    	for(j=0;j<m;j++){
+    		arr_mul[i][j]=arr_1[i][j]*arr_2[i][j];
+		}
+	}
+	
+	printf("multiplication of two matrix is:\n");
+	for(i=0;i<n;i++){
+    	for(j=0;j<m;j++){
+    		printf("%d\t",arr_mul[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
